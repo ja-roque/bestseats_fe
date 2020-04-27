cd9ca4ed9116e24f176cee29ccb7f008a7a21627
Javier <jarb95@gmail.com>
2020-04-26 21:49:40 -0600

    Add seat component

---
+++ src/components/seat/index.js
@@ -0,0 +1,2 @@
+import Seat from "./seat"
+export default Seat;
---
+++ src/components/seat/seat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './seat.scss'
+
+const Seat = (props) => {
+  let color = props.available ? '#00c0ff' : 'gray'
+  if (props.best){ color = 'green'}
+  return <div key={'e'}  className="component-seat" style={{backgroundColor: color, gridColumn: props.col + 1, gridRow: props.row + 1}}/>
+}
+
+export default Seat
